0c32c09fb8d468dba08dd191e9691a601174d7df
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2024-11-14 22:34:41 -0800

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,9 +1,54 @@
 local config = lib.require 'config'
-
+local truckDrivers = {}
 local function generateRandomModel(listLenght)
     return math.random(1, listLenght)
 end
 
+local function payment()
+    
+end
+
+lib.callback.register('lonf:trucker:clockOut', function (source)
+
+    local src = source
+    local Player = exports.qbx_core:GetPlayer(src)
+    local pos = GetEntityCoords(GetPlayerPed(src))
+
+    if truckDrivers[src].delivered then
+        print("Entrega os items")
+    else
+        print("Não entrega os items e excloi os veiculos")
+    end 
+
+
+    if not Player and #(pos - config.coords) > 5 then 
+        print("TOMA BAN")
+        return true 
+    else
+        print("NAO TOMA BAN")
+        return false
+    end
+end)
+
+lib.callback.register('lonf:trucker:clockIn', function (source)
+    local src = source
+
+    truckDrivers[src] = {
+        delivered = true
+    }
+
+    return truckDrivers[src].delivered
+end)
+
+lib.callback.register('lonf:trucker:deleteEntity', function(source, netId)
+    local entity = NetworkGetEntityFromNetworkId(netId)
+    if DoesEntityExist(entity) then
+        DeleteEntity(entity)
+        return true
+    end
+
+    return false
+end)
 
 lib.callback.register('lonf:trucker:spawnTrailer', function(source)
     local index = generateRandomModel(#config.trailers)
